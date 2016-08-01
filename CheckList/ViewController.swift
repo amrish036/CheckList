c6452f8e5acb827213902610d3e7a75c925fcc8a
amrish036 <amrish.036@gmail.com>
2016-08-01 19:48:50 +1000

    Initial Commit

---
+++ CheckList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CheckList
+//
+//  Created by Amrish Mahesh on 1/08/2016.
+//  Copyright © 2016 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
