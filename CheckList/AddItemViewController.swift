03088641f08354d84b04e6fac839f4aa69aa88f7
amrish036 <amrish.036@gmail.com>
2016-08-02 17:39:35 +1000

    c3 action for cancel and done buttons

---
+++ CheckList/AddItemViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AddItemViewController.swift
+//  CheckList
+//
+//  Created by Amrish Mahesh on 2/08/2016.
+//  Copyright © 2016 Amrish Mahesh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddItemViewController: UITableViewController{
+
+  @IBAction func cancel() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  @IBAction func done() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+}
