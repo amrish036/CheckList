2669808b3b86420fdb0548e65d89b46dfd8d8d9c
amrish036 <amrish.036@gmail.com>
2016-08-08 17:50:35 +1000

    c7-added viewcontroller for multiple checklist and storyboard seque

---
+++ CheckList/AllListViewController.swift
@@ -0,0 +1,81 @@
+//
+//  AllListViewController.swift
+//  CheckList
+//
+//  Created by Amrish Mahesh on 8/08/2016.
+//  Copyright © 2016 Amrish Mahesh. All rights reserved.
+//
+
+import UIKit
+
+
+
+class AllListViewController: UITableViewController {
+  
+  var lists: [Checklist]
+
+  required init?(coder aDecoder: NSCoder) {
+    lists = [Checklist]()
+    super.init(coder: aDecoder)
+    var list = Checklist(name: "Birthdays")
+    lists.append(list)
+    list = Checklist(name: "Groceries")
+    lists.append(list)
+    list = Checklist(name: "Cool Apps")
+    lists.append(list)
+    list = Checklist(name: "To Do")
+    lists.append(list)
+  }
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+  
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 3
+    }
+
+  
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+   
+    let cell = cellForTableView(tableView)
+    cell.textLabel!.text = "List \(indexPath.row)"
+
+        return cell
+    }
+  
+  func cellForTableView(tableView: UITableView) -> UITableViewCell {
+    let cellIdentifier = "Cell"
+    if let cell =
+      tableView.dequeueReusableCellWithIdentifier(cellIdentifier) {
+      return cell
+    } else {
+      return UITableViewCell(style: .Default,
+                             reuseIdentifier: cellIdentifier)
+    }
+  }
+  
+  
+  override func tableView(tableView: UITableView,
+                          didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    performSegueWithIdentifier("ShowChecklist", sender: nil)
+  }
+
+  
+}
